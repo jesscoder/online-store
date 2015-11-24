bdaa44d88d3eedfa676a5ea500055399deb96b1c
Jessica Van Rooyen <jessicav556@gmail.com>
2015-11-24 15:49:46 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+	require( "dbconnect.php" );
+	
+	$productList = array();
+	
+	$sql = "SELECT * FROM productList";
+	$results = mysql_query( $sql );
+	
+	while ( $myrow = mysql_fetch_array( $results ) ) {
+		$catalog[] = array(
+			"thumbnail" => $myrow['image_sm'],
+			"id" => $myrow['id'],
+			"name" => htmlentities( $myrow['name'] ),
+			"price" => $myrow['price'],
+			"excerpt" => htmlentities( $myrow['desc_short'] ),
+		);
+	}
+	
+	echo json_encode( array( 'productList' => $productList ) );
+?>
