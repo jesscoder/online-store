4047d4a2e050efd236eded9ad23411a662bec5bd
Jessica Van Rooyen <jessicav556@gmail.com>
2015-12-14 18:12:43 +0200

    Update show_catalog.php

--- show_catalog.php
+++ show_catalog.php
@@ -1,14 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
-		<title>Online Store</title>
-		<link href="css/style.css" rel="stylesheet" type="text/css" />
+		<meta http-equiv="Content-Type" content="text/html"/>
+		<meta charset="UTF-8" />
 		
+		<title>Catalog</title>
+		<link href="css/reset.css" rel="stylesheet" type="text/css" />
+		<link href="css/style.css" rel="stylesheet" type="text/css" />
 		<link href="css/bootstrap.min.css" rel="stylesheet">
+		
+		<link href='https://fonts.googleapis.com/css?family=Great+Vibes|Lato:400,100,300|Oswald|Lobster|Kaushan+Script' rel='stylesheet' type='text/css'>
+		
 		 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+		 
 	</head>
 <body>
+	
+	
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
       	<div class="navbar-header">
@@ -24,9 +32,10 @@
     </div>
 </nav>
 
+
 <div class="container catalog">
 	<div id="wrap">
-
+		<h2 class="list">Catalog List</h2>
 	<?php
 			require( "dbconnect.php" );
 			$sql = "SELECT * FROM productTable";
@@ -34,33 +43,30 @@
 			while ( $myrow = mysql_fetch_array( $results ) ) {
 			
 				$image_sm   = $myrow['image_sm'];
-				$id         = $myrow['id'];
+				$id         = $myrow['ID'];
 				$name       = $myrow['name'];
 				$price      = $myrow['price'];
 				$desc_short = $myrow['desc_short'];
-				$reviews	= $myrow['review'];
-				$rating 	= round( $myrow['rating'], 2);
+				$reviews	= $myrow['reviews'];
+				$rating 	= round( $myrow['rating']);
 				
 				echo "<div class='item'>
 			<img src='$image_sm' />
 				<p class='name'><a href='product_detail.php?cat_id=$id'>$name </a></p>
 				<p class='price_sm'>\$$price</p>
 				<p class='short'>$desc_short</p>
-				<p class='review'>Reviews:$reviews Rating: $rating</p>
+				<p class='review'>Reviews: $reviews Rating: $rating</p>
 			</div>\n";
+			if( ($count % 2 ) == 2) {
+						echo "<hr />";
+					}
+
 			}
 		?>
-
+</div>
 	</div>
 </div>
 
 
 </body>
 </html>
-Things that don't work:
-
-Long description image.
-Name, price, and description on long description page.
-Reviews/Rating results on catalog page and long description page.
-You can add a review on the long description's page, it says it's been added, but 1.) "Thanks for reviewing (should show product name)." doesn't show. 2.) No review is added to the database. 
-I've gone through this a few times, and also copied the module's example exactly, then did it over again, I still can't figure out why these things won't show. My brain is starting to fizzle. A second set of eyes can probably point out mistakes I just can't see right now. 
